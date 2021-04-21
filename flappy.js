b3734524bc0dcc00743ebb4f1483f8a0b78f129f
katarzyna_w <katarzyna.koch@gmail.com>
2021-04-20 20:25:22 -0500

    add variables to flappy.js

---
+++ flappy.js
@@ -0,0 +1,5 @@
+var obstacle = document.getElementById("obstacle")
+var obstacle2 = document.getElementById("obstacle2")
+var player = document.getElementById("player")
+var jumping = 0;
+var counter = 0;
