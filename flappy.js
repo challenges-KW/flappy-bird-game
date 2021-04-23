bcb821b3dd12e0a74aeb41a6a34529ef5228649b
katarzyna_w <katarzyna.koch@gmail.com>
2021-04-23 15:18:00 -0500

    variables in js

--- flappy.js
+++ flappy.js
@@ -1,5 +1,10 @@
-var obstacle = document.getElementById("obstacle")
-var obstacle2 = document.getElementById("obstacle2")
-var player = document.getElementById("player")
-var jumping = 0;
-var counter = 0;
+let obstacle = document.getElementById("obstacle")
+let obstacle2 = document.getElementById("obstacle2")
+let player = document.getElementById("player")
+let jumping = 0;
+let gravity = 1.5;
+let score = 0;
+let playerX = 10;
+let playerY = 100;
+
+document.addEventListener("keydown", jumpUp);
