6134d17f34278b5b80c8a96c79d250a43b405c72
katarzyna_w <katarzyna.koch@gmail.com>
2021-04-23 17:00:07 -0500

    add gravity to startGame

--- flappy.js
+++ flappy.js
@@ -1,20 +1,23 @@
-let obstacle = document.getElementById("obstacle")
-let obstacle2 = document.getElementById("obstacle2")
-let player = document.getElementById("player")
-let jumping = 0;
-let gravity = 1.5;
-let score = 0;
-let playerX = 10;
-let playerY = 100;
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    let obstacle = document.getElementById("obstacle")
+    let obstacle2 = document.getElementById("obstacle2")
+    let player = document.getElementById("player")
+    let jumping = 0;
+    let gravity = 1.5;
+    let score = 0;
+    let playerX = 10;
+    let playerY = 100;
 
     let playerLeft = 300
     let playerBottom = 0
 
     function startGame() {
+        playerBottom -= gravity
         player.style.bottom = playerBottom + 'px'
         player.style.left = playerLeft + 'px'
     }
-    startGame()
+    
+    let timer = setInterval(startGame, 20)
 });
