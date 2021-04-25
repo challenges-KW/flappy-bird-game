f196c34f94e70d82cc33d8afd8c8f9e133747a8e
katarzyna_w <katarzyna.koch@gmail.com>
2021-04-24 19:55:45 -0500

    add new bg image and create div for obstacle in flappy.js

--- flappy.js
+++ flappy.js
@@ -1,13 +1,12 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-    let obstacle = document.getElementById("obstacle")
-    let obstacle2 = document.getElementById("obstacle2")
     let player = document.getElementById("player")
+    let bg = document.getElementById("bg")
 
     let gravity = 1.5;
-    let score = 0;
-    let playerLeft = 300
-    let playerBottom = 50
+    let playerLeft = 300;
+    let playerBottom = 50;
+    let isGameOver = false;
+    let gap = 430
 
     function startGame() {
         playerBottom -= gravity
@@ -15,13 +14,48 @@ document.addEventListener("DOMContentLoaded", () => {
         player.style.left = playerLeft + 'px'
     }
     
-    let timer = setInterval(startGame, 20)
+    let startGameTimer = setInterval(startGame, 20)
 
-    function jump() {
-        if (playerBottom < 275) playerBottom += 50
-        player.style.bottom = playerBottom + 'px'
-        console.log('this is player bottom: ', playerBottom)
+    function control(e) {
+        if (e.keyCode === 38) {
+            jump()
+        }
     }
 
-    document.addEventListener('keyup', jump)
+    function jump() {
+        if (playerBottom < 245) playerBottom += 50
+        player.style.bottom = playerBottom + 'px'
+        console.log('This is playerBottom: ', playerBottom)
+    }
+    document.addEventListener('keyup', control)
+
+
+    function generateObstacle() {
+        let obstacleLeft = 1300
+        let randomHeight = Math.random() * 50
+        let obstacleBottom = randomHeight
+        const obstacle = document.createElement('div')
+        obstacle.classList.add('obstacle')
+        bg.appendChild(obstacle)
+        obstacle.style.left = obstacleLeft + 'px'
+        obstacle.style.bottom = obstacleBottom + 'px'
+
+        function moveObstacle() {
+            obstacleLeft -=2
+            obstacle.style.left = obstacleLeft + 'px'
+
+            if (obstacleLeft === -75) {
+                clearInterval(obstacleTimer)
+                bg.removeChild(obstacle)
+            }
+            if (playerBottom === 0) {
+                isGameOver()
+            }
+        }
+        let obstacleTimer = setInterval(moveObstacle, 20)
+        setTimeout(generateObstacle, 6000)
+    }
+    generateObstacle()
+    
+
 });
