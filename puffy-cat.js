62d312551f979e09512c9a56f40d2d324d59cad5
katarzyna_w <katarzyna.koch@gmail.com>
2021-05-31 09:27:59 -0500

    add puffy sprite

--- puffy-cat.js
+++ puffy-cat.js
@@ -1,10 +1,15 @@
+const puffySprite = new Image();
+puffySprite.src = 'tiny-puffy-1.gif';
+
 class Puffy {
     constructor() {
         this.x = 150;
         this.y = 200;
         this.velocity = 0;
-        this.width = 20;
-        this.height = 20;
+        this.originalWidth = 150;
+        this.originalHeight = 94;
+        this.width = this.originalWidth/1.5;
+        this.height = this.originalHeight/1.5;
         this.weight = 0.005;
     }
     update(){
@@ -25,7 +30,8 @@ class Puffy {
     }
     draw(){
         ctx.fillStyle = 'red';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        // ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(puffySprite, 0, 0, this.originalWidth, this.originalHeight, this.x - 20, this.y - 12, this.width, this.height);
     }
     jump(){
         this.velocity -= 2;
