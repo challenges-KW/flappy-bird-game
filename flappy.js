9b94b38b902c8725ed000d893672c44200d00146
katarzyna_w <katarzyna.koch@gmail.com>
2021-06-02 13:08:08 -0500

    fonts to bungee and bungee shade

--- flappy.js
+++ flappy.js
@@ -9,6 +9,12 @@ let frame = 0;
 let score = 0;
 let hue = 0;
 let gameSpeed = 2;
+let restartButton = document.getElementById("restartButton");
+
+restartButton.addEventListener("click", (e) => {
+    console.log("restart")
+});
+
 
 const background = new Image();
 background.src = "bg-puffy.png";
@@ -36,9 +42,10 @@ function animate() {
     puffy.update();
     puffy.draw();
     ctx.fillStyle = '#00396c';
-    ctx.font = "90px Poppins";
+    ctx.font = '90px "Bungee Shade"';
     ctx.strokeText(score, 450, 80);
     ctx.fillText(score, 450, 80);
+    gameStart();
     handleCollisions();
     if (handleCollisions()) return;
     requestAnimationFrame(animate);
@@ -67,126 +74,19 @@ function handleCollisions(){
             puffy.y + puffy.height < canvas.height))){
                 //collision occurs
                 ctx.drawImage(collide, puffy.x, puffy.y, 50, 50);
-                ctx.font = "25px Poppins";
-                ctx.fillStyle = 'black';
-                ctx.fillText = ('Game Over, your score is ' + score, 160, canvas.height/2 - 10);
-                return true
+                ctx.font = '25px Bungee';
+                ctx.fillStyle = 'slateblue';
+                ctx.fillText('Game Over!', canvas.width/2 - 80, canvas.height/2 - 10);
+
+                return true;
             }
     }
 }
 
+function gameStart() {
+    ctx.save();
+}
 
-// document.addEventListener("DOMContentLoaded", () => {
-
-
-    // let container = document.getElementById("container")
-    // let player = document.getElementById("player")
-    // let bgSky = document.getElementById("bg-sky")
-
-    // let gravity = 2.5;
-    // let playerLeft = 100;
-    // let playerBottom = 50;
-    // let isGameOver = false;
-    // let gap = 430
-    
-
-    // let startBtn = document.querySelector("#startBtn")
-
-    // startBtn.addEventListener("click", function () {
-    //     startBtn.style.display = "none";
-    //     player.classList.remove("playerDisappear")
-    //     player.classList.add("playerShow")
-    //     let gravityTimer = setInterval(startGame, 20)
-    //     function startGame() {
-    //         playerBottom -= gravity
-    //         player.style.bottom = playerBottom + 'px'
-    //         player.style.left = playerLeft + 'px'
-    //     }
-    
-    // //continous gravity on player
-    
-    //     function control(e) {
-    //         if (e.keyCode === 38) {
-    //             jump()
-    //         }
-    //     }
-    
-    //     function jump() {
-    //         if (playerBottom < 0) playerBottom += 50
-    //         player.style.bottom = playerBottom + 'px'
-    //         console.log('This is playerBottom and playerLeft: ', playerBottom, " , ", playerLeft)
-    
-    //     }
-    //     document.addEventListener('keyup', control)
-    
-    
-    //     function generateObstacle() {
-    //         let obstacleLeft = 550
-    //         let randomHeight = Math.random() * 70
-    //         let obstacleBottom = randomHeight
-    //         console.log('obstacle bottom and obstacle left on generate: ', obstacleBottom, obstacleLeft)
-    //         const obstacle = document.createElement('div')
-    //         const topObstacle = document.createElement('div')
-    //         if (!isGameOver) {
-    //             obstacle.classList.add('obstacle')
-    //             topObstacle.classList.add('topObstacle')
-    //         }
-    //         container.appendChild(obstacle)
-    //         container.appendChild(topObstacle)
-    //         obstacle.style.left = obstacleLeft + 'px'
-    //         topObstacle.style.left = obstacleLeft + 'px'
-    //         obstacle.style.bottom = obstacleBottom + 'px'
-    //         topObstacle.style.bottom = obstacleBottom + gap + 'px'
-    
-    //         function moveObstacle() {
-    //             obstacleLeft -=2
-    //             obstacle.style.left = obstacleLeft + 'px'
-    //             topObstacle.style.left = obstacleLeft + 'px'
-    
-    //             if (obstacleLeft === -75) {
-    //                 clearInterval(obstacleTimer)
-    //                 container.removeChild(obstacle)
-    //                 container.removeChild(topObstacle)
-    //             }
-                
-    //             if (
-    //                 //not in last 200 pixels of travels
-    //                 obstacleLeft > -240 && obstacleLeft < -160 && playerLeft === 220
-    //                 || playerBottom === -440
-    //                 // (
-    //                 // playerBottom < obstacleBottom + 150 || 
-    //                 // playerBottom > obstacleBottom + gap - 200 ||
-    //                 // playerBottom === -440
-    //                 // )
-    //                 ) {
-    //                     console.log('obstacle bottom and obstacle left: ', obstacleBottom, ' , ' , obstacleLeft)
-    //                     console.log('player bottom and player left :', playerBottom, playerLeft)
-                 
-    //                 gameOver()
-    //                 clearInterval(obstacleTimer)
-    //             }
-                
-    //         }
-    //         let obstacleTimer = setInterval(moveObstacle, 20)
-    //         if(!isGameOver) setTimeout(generateObstacle, 6000)
-    //     }
-    //     generateObstacle()
-
-    //     function score() {
-            
-    //     }
-
-    //    function gameOver() {
-    //        clearInterval(gravityTimer)
-    //        isGameOver = true
-    //        console.log('game over')
-    //        player.classList.remove("playerShow")
-    //     //    player.classList.add("playerDisappear")   
-    //        document.removeEventListener('keyup', control)
-    //        const gameOverMessage = document.createElement('div')
-    //        gameOverMessage.classList.add("over")
-    //        gameOverMessage.innerText = "Game over! Click here to try again!"
-    //    }
-    
-    // })
-
+function gameRestart() {
+    ctx.restore();
+}
