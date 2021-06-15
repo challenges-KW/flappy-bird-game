18e03eecb3d3b4c77ab875bb8c61d1d18e88e9fb
katarzyna_w <katarzyna.koch@gmail.com>
2021-06-14 22:35:00 -0500

    obstacle color to medium orchid to match bg

--- obstacles.js
+++ obstacles.js
@@ -6,7 +6,7 @@ class Obstacle {
         this.bottom = (Math.random() * canvas.height/3) + 20;
         this.x = canvas.width;
         this.width = 20;
-        this.color = 'orchid';
+        this.color = 'mediumorchid';
         this.counted = false;
         this.border = 'papayawhip';
     }
